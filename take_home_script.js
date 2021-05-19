443145ab61c5f903ea6cd6a0db265e266efee142
root <root@kali.tgkali.com>
2021-05-19 10:28:11 -0700

    third

---
+++ take_home_script.js
@@ -0,0 +1,8 @@
+document.write("// This is a single line JavaScript comment");
+
+document.write("/* this is multiple line commenting */");
+
+console.log("My name is Tsehaye Gebreamlak");
+console.log("my favorite food is fruit for now lol");
+
+
